72e832f0a2d980c91ba10af5c6cc4061513e75a5
Islam Shadeed <37776598+isllamm@users.noreply.github.com>
2023-03-02 14:42:04 +0200

    NotesViewModel

---
+++ app/src/main/java/com/isllam/noteapp/feature_note/presentation/notes/NotesState.kt
@@ -0,0 +1,13 @@
+package com.isllam.noteapp.feature_note.presentation.notes
+
+import com.isllam.noteapp.feature_note.domain.model.Note
+import com.isllam.noteapp.feature_note.domain.util.NoteOrder
+import com.isllam.noteapp.feature_note.domain.util.OrderType
+
+
+data class NotesState(
+    val notes: List<Note> = emptyList(),
+    val noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+    val isOrderSectionVisible: Boolean = false
+
+)
